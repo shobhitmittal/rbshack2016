5114eb17c9f3d2fad711585daab286cf3cf1294f
root <root@localhost.localdomain>
2016-05-14 18:13:27 +0530

    Minor commits and a example speech processing script

--- req/req.sh
+++ req/req.sh
@@ -1,1 +1,4 @@
-sudo apt-get install python-dev python-pip
+sudo apt-get install python-dev python-pip python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose libasound-dev
+
+http://stackoverflow.com/questions/20023131/cannot-install-pyaudio-gcc-error
+http://portaudio.com/archives/pa_stable_v19_20140130.tgz
