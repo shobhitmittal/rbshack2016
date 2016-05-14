21d91865309c69c84a6e7d5d28866389a5e32203
shobhitmittal <shobhitmittal250@gmail.com>
2016-05-14 17:33:08 -0400

    test url

--- rbshack/rbs/views.py
+++ rbshack/rbs/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect, HttpResponse 
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader, Context
+import models
+from django.conf import settings
+import os
+import json
+import time
+import re
+from lxml import etree
 
-# Create your views here.
+def landing(request):
+	my_context={}
+	return render_to_response('home.html',my_context,context_instance=RequestContext(request))
+
+def login(request):
+	print request.method
+	print request.POST
+	return HttpResponse('HSAISbni')
+
+def test(request):
+	print request
+	return HttpResponse('sdfds')
--- rbshack/rbshack/settings.py
+++ rbshack/rbshack/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DATA_PATHS={'audio':'/tmp/audio','video':'/tmp/video'}
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rbs',
 ]
 
 MIDDLEWARE_CLASSES = [
